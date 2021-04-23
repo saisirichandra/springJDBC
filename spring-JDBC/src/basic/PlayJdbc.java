e55bad456354a8d8aac04eca21611c572d92d409
saisirichandra <sirichandrapodugu@gmail.com>
2021-04-23 11:29:44 +0530

    Insert

---
+++ spring-JDBC/src/basic/PlayJdbc.java
@@ -0,0 +1,19 @@
+package basic;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import basic.dao.StudentDao;
+import basic.dao.StudentDaoImpl;
+import basic.model.Student;
+public class PlayJdbc {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Student myStudent = new Student(12,"Chandra", 1, 20);
+		//Student anotherStudent = new Student(14,"chandra",6,88);
+		StudentDao studentDao = (StudentDao) context.getBean("studentDao");
+		//= new StudentDaoImpl();
+		studentDao.insert(myStudent);
+	}
+
+}
---
+++ spring-JDBC/src/basic/dao/StudentDaoImpl.java
@@ -0,0 +1,37 @@
+package basic.dao;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import basic.model.Student;
+
+public class StudentDaoImpl  implements StudentDao{
+	private JdbcTemplate jdbcTemplate ;
+	
+	
+
+
+
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+
+
+
+	@Override
+	public void insert(Student student) {
+		//INSERT INTO `student` (`_id`, `name`, `semester`, `average`) 
+		//VALUES ('9', 'yahas', '2', '67');
+		String sql = "INSERT INTO student VALUES (?,?,?,?)";
+		Object[] objects =	{student.getId(),student.getName(),student.getSem(),student.getAverage()};
+
+		int no_rows_inserted =	jdbcTemplate.update(sql,objects);
+		System.out.println("no of rows inserted  is"+ no_rows_inserted);
+	}
+
+
+	
+}
